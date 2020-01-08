2824d1df5cb1872169d76414211cd1214b8f69c3
DavideBicego <davide.bicego@sintesia.com>
2020-01-08 11:47:54 +0100

    fix: rotation was showing half screen on iOS

--- ios/Classes/ScannerOverlay.m
+++ ios/Classes/ScannerOverlay.m
@@ -85,11 +85,19 @@
   - (CGRect)scanRect {
     CGRect rect = self.frame;
     CGFloat heightMultiplier = 3.0/4.0; // 4:3 aspect ratio
-    CGFloat scanRectWidth = rect.size.width * 0.8f;
-    CGFloat scanRectHeight = scanRectWidth * heightMultiplier;
-    CGFloat scanRectOriginX = (rect.size.width / 2) - (scanRectWidth / 2);
-    CGFloat scanRectOriginY = (rect.size.height / 2) - (scanRectHeight / 2);
-    return CGRectMake(scanRectOriginX, scanRectOriginY, scanRectWidth, scanRectHeight);
+    if(rect.size.width < rect.size.height){
+        CGFloat scanRectWidth = rect.size.width * 0.8f;
+        CGFloat scanRectHeight = scanRectWidth * heightMultiplier;
+        CGFloat scanRectOriginX = (rect.size.width / 2) - (scanRectWidth / 2);
+        CGFloat scanRectOriginY = (rect.size.height / 2) - (scanRectHeight / 2);
+        return CGRectMake(scanRectOriginX, scanRectOriginY, scanRectWidth, scanRectHeight);
+    }else{
+        CGFloat scanRectHeight = rect.size.height * 0.8f;
+        CGFloat scanRectWidth = scanRectHeight * heightMultiplier;
+        CGFloat scanRectOriginX = (rect.size.width / 2) - (scanRectWidth / 2);
+        CGFloat scanRectOriginY = (rect.size.height / 2) - (scanRectHeight / 2);
+        return CGRectMake(scanRectOriginX, scanRectOriginY, scanRectWidth, scanRectHeight);
+    }
   }
   
   - (CGRect)scanLineRect {
